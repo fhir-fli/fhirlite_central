9fd6b50e990b48af7fa85fb761f06662822a5b1c
Dokotela <grey.faulkenberry@mayjuun.com>
2023-05-23 08:58:39 -0400

    Need to think on this

---
+++ server_files/listening_controller.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:fhir/r4.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf_router/shelf_router.dart';
+
+class ListeningController {
+  ///Define our getter for our handler
+  Handler get handler {
+    final router = Router();
+
+    /// main post route (acts the same as put), first gets the resource info
+    /// for the new Resource. As long as that is valid, get the past from the
+    /// URL, and as long as that exists, pass it onto the post function
+    router.post('/', (Request request) async {
+      final requestString = await request.readAsString();
+      return Response.ok('Post Request made, but payload incorrect');
+    });
+
+    router.post('/fhir/', (Request request) async {
+      final requestString = await request.readAsString();
+      final resource = Resource.fromJson(jsonDecode(requestString));
+      return Response.ok('Post Request made, but payload incorrect');
+    });
+
+    router.post('/fhir', (Request request) async {
+      final requestString = await request.readAsString();
+      final resource = Resource.fromJson(jsonDecode(requestString));
+      return Response.ok('Post Request made, but payload incorrect');
+    });
+
+    ///You can catch all verbs and use a URL-parameter with a regular expression
+    ///that matches everything to catch app.
+    router.all('/<ignored|.*>', (Request request) {
+      // return Response.notFound('Page not found');
+      return Response.ok('Page not found');
+    });
+
+    return router;
+  }
+}
---
+++ server_files/server_files.dart
@@ -0,0 +1,1 @@
+export 'listening_controller.dart';
